4a235b1af2d93170880bb89868ab6f948eebc2bf
Jvictorifran <joaovictorcandidoifran@gmail.com>
2025-09-17 20:19:34 -0400

    atualizaçao parte 1

--- dino.py
+++ dino.py
@@ -4,6 +4,8 @@ from sys import exit
 import os
 
 diretorio_principal = os.path.dirname(__file__)
+diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
+diretorio_sons = os.path.join(diretorio_principal, 'sons')
 
 #definindo os tamanhos da janela
 LARGURA = 640
@@ -13,16 +15,35 @@ ALTURA = 480
 BRANCO = (255,255,255)
 
 #vairaveis da janela
+# inicializa os módulos do pygame
+
+# cria a janela
 tela = pygame.display.set_mode((LARGURA, ALTURA))
-pygame.display.set__caption("Jogo do Dino")
+pygame.display.set_caption("Jogo do Dino")
 
 #classes, variaveis e sprites
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, "dinoSpritesheet.png")).convert_alpha()
+
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        pass
+        self.image_dinossauro = []
+        for i in range(3):
+            img =  sprite_sheet.subsurface((i*32, 0), (32, 32))
+            self.image_dinossauro.append(img)
 
-todas_as_sprites = pygame.sprite.Group
+        self.index_lista = 0
+        self.image = self.image_dinossauro[self.index_lista]
+        self.rect = self.image.get_rect()
+        self.rect.center = (100, 100)
+
+    def update(self):
+        if self.index_lista > 2:
+            self.index_lista = 0
+        self.index_lista += 0.25
+        self.image = self.image_dinossauro[int(self.index_lista)]
+
+todas_as_sprites = pygame.sprite.Group()
 dino = Dino()
 todas_as_sprites.add(dino)
 
@@ -36,3 +57,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+   
+    todas_as_sprites.draw(tela)
+    todas_as_sprites.update()
+
+    pygame.display.flip()
