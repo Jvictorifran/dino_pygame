4ee8fb6e2b91c30679c58ce26619a9f4d50963f7
Jvictorifran <joaovictorcandidoifran@gmail.com>
2025-10-01 19:59:12 -0400

    adicionando colisao

--- dino.py
+++ dino.py
@@ -25,6 +25,10 @@ pygame.display.set_caption("Jogo do Dino")
 #classes, variaveis e sprites
 sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, "dinoSpritesheet.png")).convert_alpha()
 
+som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'death_sound.wav'))
+
+colidiu = False
+#classe do dinossauro
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -39,10 +43,12 @@ class Dino(pygame.sprite.Sprite):
         self.index_lista = 0
         self.image = self.image_dinossauro[self.index_lista]
         self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(img)
         self.pos_y_inicial = ALTURA - 64 - 96//2
         self.rect.center = (100, ALTURA - 64)
         self.pulo = False
 
+    #metodo de update da classe dino
     def update(self):
         if self.pulo == True:
             if self.rect.y <= 200:
@@ -57,12 +63,12 @@ class Dino(pygame.sprite.Sprite):
             self.index_lista = 0
         self.index_lista += 0.25
         self.image = self.image_dinossauro[int(self.index_lista)]
-
-    
+    #metodo de pular da classe dino
     def pular(self):
         self.pulo = True
         self.som_pulo.play()
 
+#classe das nuvens
 class Nuvens(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -72,12 +78,14 @@ class Nuvens(pygame.sprite.Sprite):
         self.rect.y = randrange(50, 200, 50)
         self.rect.x = LARGURA- randrange(30, 300, 90)
     
+    #metodo de update da classe nuvem
     def update(self):
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA 
             self.rect.y = randrange(50, 200, 50)
         self.rect.x -= 10
 
+#classe do chão 
 class Chao(pygame.sprite.Sprite):
     def __init__(self, pos_x):
         pygame.sprite.Sprite.__init__(self)
@@ -87,24 +95,55 @@ class Chao(pygame.sprite.Sprite):
         self.rect.y = ALTURA - 64
         self.rect.x = pos_x * 64
 
+    #update da classe chao
     def update (self):
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA 
         self.rect.x -= 10
+    
+#classe cacto
+class Cacto(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)  
+        self.image = sprite_sheet.subsurface((5*32, 0), (32,32))
+        self.image = pygame.transform.scale(self.image, (32*2, 32*2))  
+        self.rect = self.image.get_rect() 
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = (LARGURA, ALTURA-64)
 
+    #update da classe cacto
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = LARGURA
+        else:
+            self.rect.x -=10
+
+#aqui ficam todas as sprites do jogo
 todas_as_sprites = pygame.sprite.Group()
+
+#armazenamento ca classe dino
 dino = Dino()
+todas_as_sprites.add(dino)
+
+#armazenamento do objeto nuvem
 for i in range(4):  
     nuvem = Nuvens()
     todas_as_sprites.add(nuvem)
 
+#armazenamento do objeto chao
 for i in range(20):
     chao = Chao(i)
     todas_as_sprites.add(chao)
 
-todas_as_sprites.add(dino)
+#armazenamento do objeto cacto
+cacto = Cacto()
+todas_as_sprites.add(cacto)
 
+#grupo dos obsctaculos
+grupo_obstaculos = pygame.sprite.Group()
+grupo_obstaculos.add(cacto)
 
+#fps 
 relogio = pygame.time.Clock()
 
 # loop principal do jogo
@@ -115,6 +154,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+
         if event.type == KEYDOWN:
             if event.key == K_SPACE:
                 if dino.rect.y != dino.pos_y_inicial:
@@ -122,7 +162,17 @@ while True:
                 else:
                     dino.pular()
 
+    colisoes = pygame.sprite.spritecollide(dino, grupo_obstaculos, False, pygame.sprite.collide_mask)
     todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
+
+    if colisoes and colidiu == False:
+        som_colisao.play()
+        colidiu = True
+        
+    if colidiu == True:
+        pass
+    
+    else:
+        todas_as_sprites.update()
 
     pygame.display.flip()
